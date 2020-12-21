ddd97e234207e21b4c5dc974e5c7e9ef4e1b142b
unknown <qinxuechao@yahoo.com>
2020-12-22 00:13:22 +0800

    add git ignore

--- ch5/findlinks1.go
+++ ch5/findlinks1.go
@@ -20,6 +20,15 @@ func main() {
 }
 
 func visit(links []string, n *html.Node) []string {
-
-
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == 'href'{
+			links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
 }
