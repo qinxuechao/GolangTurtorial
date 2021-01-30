d7337e6bf3bed2a3d13a5ce522b29c6735aee041
Xuechao Qin <xcqin@aibee.com>
2021-01-31 02:55:06 +0800

    commit

---
+++ ch8/du1/du1.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
+// walkdir 递归地遍历以 dir 为根目录的整个文件数
+// 并在fileSizes上发送每个已经找到的文件的大小
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+		return nil
+	}
+	return entries
+}
+
+func main() {
+	// 确定初始目录
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	// 遍历文件树
+	fileSizes := make(chan int64)
+	go func() {
+		for _, root := range roots {
+			walkDir(root, fileSizes)
+		}
+		close(fileSizes)
+	}()
+
+	//// 输出结果
+	//var nfiles, nbytes int64
+	//for size := range fileSizes {
+	//	nfiles++
+	//	nbytes += size
+	//}
+	//printDiskUsage(nfiles, nbytes)
+
+	// 定期输出结果
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes) // final totals
+}
+
+func printDiskUsage(nfiles int64, nbytes int64) {
+	fmt.Printf("%d fukes %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
---
+++ ch8/du3/du3.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var sema = make(chan struct{}, 20) // concureency-limiting counting semaphore
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
+// walkdir 递归地遍历以 dir 为根目录的整个文件数
+// 并在fileSizes上发送每个已经找到的文件的大小
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
+	if cancelled() {
+		return
+	}
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			n.Add(1)
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, n, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+// dirents returns the entries of directory dir
+func dirents(dir string) []os.FileInfo {
+	select {
+	case sema <- struct{}{}: // acqure token
+	case <-done:
+		return nil
+	}
+
+	defer func() { <-sema }() //release token
+
+	f, err := os.Open(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	defer f.Close()
+
+	entries, err := f.Readdir(0) // 0 => no limit; read all entries
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		// Don't return: Readdir may return partial results
+	}
+
+	return entries
+}
+
+func main() {
+	// 确定初始目录
+	roots := os.Args[1:]
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		close(done)
+	}()
+
+	// 遍历文件树
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
+
+	go func() {
+		n.Wait()
+		close(fileSizes)
+	}()
+
+	// 定期输出结果
+	var tick <-chan time.Time
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case <- done:
+			for range fileSizes {
+				// Do nothing
+			}
+			return
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes) // final totals
+}
+
+func printDiskUsage(nfiles int64, nbytes int64) {
+	fmt.Printf("%d fukes %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
