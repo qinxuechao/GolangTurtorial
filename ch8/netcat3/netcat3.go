11998f293a6bbba6b5dfaed936311b9e31858e91
Xuechao Qin <xcqin@aibee.com>
2021-04-28 17:41:31 +0800

    update

--- ch8/netcat3/netcat3.go
+++ ch8/netcat3/netcat3.go
@@ -8,22 +8,19 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:32005")
+	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// 此处设计成无缓冲通道，发送操作会阻塞，是顺序发送
-	// 导致发送和接收goroutine同步化
 	done := make(chan struct{})
 	go func() {
-		io.Copy(os.Stdout, conn)
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
 		log.Println("done")
-		done <- struct{}{} //指示主 goroutine
+		done <- struct{}{} //signal the main goroutine
 	}()
 	mustCopy(conn, os.Stdin)
 	conn.Close()
-	<-done // 等待后台 goroutine 完成
+	<-done // wait for background goroutine to finish
 }
 
 func mustCopy(dst io.Writer, src io.Reader) {
